b1a547cf18cb339e799cc81660e48f0834727d2c
chenzhihui <zhihui_chen@foxmail.com>
2025-10-15 18:49:05 +0800

    add tool description

--- memory/tool.go
+++ memory/tool.go
@@ -8,12 +8,12 @@ import (
 
 // Request is the request for the memory tool.
 type Request struct {
-	Query string `json:"query"`
+	Query string `json:"query" jsonschema:"The query to search the memory."`
 }
 
 // Response is the response for the memory tool.
 type Resposne struct {
-	Memories []*Memory `json:"memories"`
+	Memories []*Memory `json:"memories" jsonschema:"The memories found for the query."`
 }
 
 // NewMemoryTool creates a new memory tool with the given memory store.
