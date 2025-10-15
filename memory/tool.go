7f5c1c8cf90eed1fb1c9fad18ae948975ba08931
chenzhihui <zhihui_chen@foxmail.com>
2025-10-15 18:32:48 +0800

    add memory tool

---
+++ memory/tool.go
@@ -0,0 +1,32 @@
+package memory
+
+import (
+	"context"
+
+	"github.com/go-kratos/blades/tools"
+)
+
+// Request is the request for the memory tool.
+type Request struct {
+	Query string `json:"query"`
+}
+
+// Response is the response for the memory tool.
+type Resposne struct {
+	Memories []*Memory `json:"memories"`
+}
+
+// NewMemoryTool creates a new memory tool with the given memory store.
+func NewMemoryTool(store MemoryStore) (*tools.Tool, error) {
+	return tools.NewTool[Request, Resposne](
+		"Memory",
+		"You have memory. You can use it to answer questions. If any questions need you to look up the memory.",
+		tools.HandleFunc[Request, Resposne](func(ctx context.Context, req Request) (Resposne, error) {
+			memories, err := store.SearchMemory(ctx, req.Query)
+			if err != nil {
+				return Resposne{}, err
+			}
+			return Resposne{Memories: memories}, nil
+		}),
+	)
+}
