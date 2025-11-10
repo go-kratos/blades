196af47df7f396ea290f91cddf77a3585101f13b
chenzhihui <zhihui_chen@foxmail.com>
2025-11-10 17:08:31 +0800

    refactor tools interface

--- tools/tool.go
+++ tools/tool.go
@@ -1,20 +1,35 @@
 package tools
 
 import (
+	"context"
+
 	"github.com/google/jsonschema-go/jsonschema"
 )
 
-// Tool represents a tool with a name, description, input schema, and a tool handler.
-type Tool struct {
-	Name         string                  `json:"name"`
-	Description  string                  `json:"description"`
-	InputSchema  *jsonschema.Schema      `json:"inputSchema"`
-	OutputSchema *jsonschema.Schema      `json:"outputSchema"`
-	Handler      Handler[string, string] `json:"-"`
+type Tool interface {
+	Name() string
+	Description() string
+	InputSchema() *jsonschema.Schema
+	OutputSchema() *jsonschema.Schema
+	Handle(context.Context, string) (string, error)
 }
 
-// NewTool creates a new Tool with the given name, description, input and output types, and handler.
-func NewTool[I, O any](name string, description string, handler Handler[I, O]) (*Tool, error) {
+func NewTool(name string, description string, handler Handler[string, string], opts ...Option) Tool {
+	t := &tool{
+		name:         name,
+		description:  description,
+		inputSchema:  nil,
+		outputSchema: nil,
+		handler:      handler,
+	}
+	for _, opt := range opts {
+		opt(t)
+	}
+	return t
+}
+
+// NewToolFunc creates a new Tool with the given name, description, input and output types, and handler.
+func NewToolFunc[I, O any](name string, description string, handler Handler[I, O]) (Tool, error) {
 	inputSchema, err := jsonschema.For[I](nil)
 	if err != nil {
 		return nil, err
@@ -23,11 +38,56 @@ func NewTool[I, O any](name string, description string, handler Handler[I, O]) (
 	if err != nil {
 		return nil, err
 	}
-	return &Tool{
-		Name:         name,
-		Description:  description,
-		InputSchema:  inputSchema,
-		OutputSchema: outputSchema,
-		Handler:      JSONAdapter(handler),
+	return &tool{
+		name:         name,
+		description:  description,
+		inputSchema:  inputSchema,
+		outputSchema: outputSchema,
+		handler:      JSONAdapter(handler),
 	}, nil
+}
+
+type Option func(*tool)
+
+// WithInputSchema sets the input schema for the tool.
+func WithInputSchema(schema *jsonschema.Schema) Option {
+	return func(t *tool) {
+		t.inputSchema = schema
+	}
+}
+
+// WithOutputSchema sets the output schema for the tool.
+func WithOutputSchema(schema *jsonschema.Schema) Option {
+	return func(t *tool) {
+		t.outputSchema = schema
+	}
+}
+
+// tool represents a tool with a name, description, input schema, and a tool handler.
+type tool struct {
+	name         string
+	description  string
+	inputSchema  *jsonschema.Schema
+	outputSchema *jsonschema.Schema
+	handler      Handler[string, string]
+}
+
+func (t *tool) Name() string {
+	return t.name
+}
+
+func (t *tool) Description() string {
+	return t.description
+}
+
+func (t *tool) InputSchema() *jsonschema.Schema {
+	return t.inputSchema
+}
+
+func (t *tool) OutputSchema() *jsonschema.Schema {
+	return t.outputSchema
+}
+
+func (t *tool) Handle(ctx context.Context, input string) (string, error) {
+	return t.handler.Handle(ctx, input)
 }
